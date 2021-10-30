d962603e0a3225e0672bb82de4fd60315cbb6587
abdoutalby <88724836+abdoutalby@users.noreply.github.com>
2021-10-30 15:53:14 +0100

    Update DeviceController.java

--- src/main/java/com/abdou/esp/device/DeviceController.java
+++ src/main/java/com/abdou/esp/device/DeviceController.java
@@ -27,4 +27,8 @@ public class DeviceController {
         return this.service.add(device);
     }
 
+    @DeleteMapping
+    public ResponseEntity delete(@PathVariable Long id){
+        return  this.service.delete(id);
+    }
 }
