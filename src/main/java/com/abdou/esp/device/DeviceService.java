5a431e3d6b347ba495bd703dd58bd7b35c729965
abdoutalby <88724836+abdoutalby@users.noreply.github.com>
2021-11-05 15:57:53 +0100

    Update DeviceService.java

--- src/main/java/com/abdou/esp/device/DeviceService.java
+++ src/main/java/com/abdou/esp/device/DeviceService.java
@@ -4,9 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import springfox.documentation.spring.web.json.Json;
 
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
@@ -24,18 +23,6 @@ public class DeviceService {
     public List<Device> getAllDevices(){
       return repo.findAll();
     }
-
-    public ResponseEntity delete( Long device){
-        if(repo.existsById(device)){
-            repo.delete(repo.getById(device));
-            return  new ResponseEntity(new Json("deleted"),HttpStatus.OK);
-        }else {
-            return new ResponseEntity(new Json(  " device does not exist"),HttpStatus.OK);
-        }
-    }
-
-  
-    
     public  ResponseEntity add(Device device){
         if (repo.existsById(device.getId())){
             return new ResponseEntity ("device already exist" , HttpStatus.CONFLICT);
@@ -43,6 +30,5 @@ public class DeviceService {
             DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd.HH:mm:ss");  
             device.setStart(dtf.format(LocalDateTime.now()));
             return new ResponseEntity( repo.save(device) ,HttpStatus.CREATED) ;
-        }
-    }
+        }}
 }
